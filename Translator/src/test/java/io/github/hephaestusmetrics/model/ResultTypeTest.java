4a78cfb611fc365ad90fcb181b5821e1fa8d2f1f
Dewkong <kryniu113@gmail.com>
2022-11-16 16:26:31 +0100

    Add ResultType tests for new fromString method.

--- Translator/src/test/java/io/github/hephaestusmetrics/model/ResultTypeTest.java
+++ Translator/src/test/java/io/github/hephaestusmetrics/model/ResultTypeTest.java
@@ -1,18 +1,21 @@
 package io.github.hephaestusmetrics.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ResultTypeTest {
     @Test
     public void numberOfEnumTypes() {
         //given
-        ResultTypes[] resultTypes = ResultTypes.values();
+        ResultType[] resultTypes = ResultType.values();
 
         //when
         int numberOfResultTypes = resultTypes.length;
@@ -24,18 +27,46 @@ public class ResultTypeTest {
     @Test
     public void enumTypes() {
         //given
-        List<ResultTypes> resultTypes = Arrays.asList(ResultTypes.values());
+        List<ResultType> resultTypes = Arrays.asList(ResultType.values());
 
         //when
-        boolean scalarPresent = resultTypes.contains(ResultTypes.SCALAR);
-        boolean stringPresent = resultTypes.contains(ResultTypes.STRING);
-        boolean matrixPresent = resultTypes.contains(ResultTypes.MATRIX);
-        boolean vectorPresent = resultTypes.contains(ResultTypes.VECTOR);
+        boolean scalarPresent = resultTypes.contains(ResultType.SCALAR);
+        boolean stringPresent = resultTypes.contains(ResultType.STRING);
+        boolean matrixPresent = resultTypes.contains(ResultType.MATRIX);
+        boolean vectorPresent = resultTypes.contains(ResultType.VECTOR);
 
         //then
         assertTrue(scalarPresent);
         assertTrue(stringPresent);
         assertTrue(matrixPresent);
         assertTrue(vectorPresent);
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultTypeStringSource")
+    public void fromStringTest(String resultTypeString, ResultType expectedType) {
+        //when
+        ResultType resultType = ResultType.fromString(resultTypeString);
+
+        //then
+        assertEquals(expectedType, resultType);
+    }
+
+    @Test
+    public void fromStringShouldThrowExceptionTest() {
+        //given
+        String wrongResultTypeString = "WRONG_RESULT_TYPE_STRING";
+
+        //then
+        assertThrows(RuntimeException.class, () -> ResultType.fromString(wrongResultTypeString));
+    }
+
+    private static Stream<Arguments> resultTypeStringSource() {
+        return Stream.of(
+                Arguments.of("string", ResultType.STRING),
+                Arguments.of("scalar", ResultType.SCALAR),
+                Arguments.of("vector", ResultType.VECTOR),
+                Arguments.of("matrix", ResultType.MATRIX)
+        );
     }
 }
