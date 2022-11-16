24c1cbfb46edea97bff51d6ef5aff74aeef78fad
Dewkong <kryniu113@gmail.com>
2022-11-16 17:58:01 +0100

    Add Matrix QueryResult test. Use mocked Metric in complex QueryResults. Add ResultType assertion in complex QueryResults.

--- Translator/src/test/java/io/github/hephaestusmetrics/model/queryresults/VectorQueryResultTest.java
+++ Translator/src/test/java/io/github/hephaestusmetrics/model/queryresults/VectorQueryResultTest.java
@@ -2,31 +2,25 @@ package io.github.hephaestusmetrics.model.queryresults;
 
 import io.github.hephaestusmetrics.model.ResultType;
 import io.github.hephaestusmetrics.model.metrics.Metric;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VectorQueryResultTest {
-    private static final String TAG = "QUERY_TAG";
-    private static final ResultType RESULT_TYPE = ResultType.VECTOR;
-    private static final Map<String, String> LABELS = new HashMap<>(){{
-        put("LABEL1", "VALUE1");
-    }};
-    private static final Double TIMESTAMP = 1659261600D;
-    private static final Double VALUE = 125D;
-    private final Metric metric = new Metric(TAG, RESULT_TYPE, LABELS, TIMESTAMP, VALUE);;
+    private static final String TAG = "TAG";
+
+    @Mock
+    private Metric mockMetric;
 
     @Test
     public void sanityTest() {
         //given
-        List<Metric> metrics = new ArrayList<>(){{
-            add(metric);
+        List<Metric> metrics = new ArrayList<>() {{
+            add(mockMetric);
         }};
 
         //when
@@ -34,14 +28,9 @@ public class VectorQueryResultTest {
 
         //then
         assertEquals(TAG, vectorQueryResult.getTag());
+        assertEquals(ResultType.VECTOR, vectorQueryResult.getType());
         assertEquals(1, vectorQueryResult.getSize());
         assertEquals(metrics, vectorQueryResult.getAll());
-
-        assertEquals(TAG, vectorQueryResult.get(0).getQueryTag());
-        assertEquals(RESULT_TYPE, vectorQueryResult.get(0).getResultType());
-        assertEquals(LABELS, vectorQueryResult.get(0).getLabels());
-        assertEquals(TIMESTAMP, vectorQueryResult.get(0).getTimestamp());
-        assertEquals(VALUE.toString(), vectorQueryResult.get(0).getValueString());
-        assertEquals(VALUE, vectorQueryResult.get(0).getValue());
+        assertEquals(mockMetric, vectorQueryResult.get(0));
     }
 }
