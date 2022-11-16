0a4674e1f2da64f2833df8ba3d0b9685aba75513
Dewkong <kryniu113@gmail.com>
2022-11-16 16:09:18 +0100

    Modify structure for dataformat changes.

--- Translator/src/test/java/io/github/hephaestusmetrics/model/ResultTypeTest.java
+++ Translator/src/test/java/io/github/hephaestusmetrics/model/ResultTypeTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ResultTypesTest {
+public class ResultTypeTest {
     @Test
     public void numberOfEnumTypes() {
         //given
---
+++ Translator/src/test/java/io/github/hephaestusmetrics/model/queryresults/RawQueryResultTest.java
@@ -0,0 +1,4 @@
+package io.github.hephaestusmetrics.model.queryresults;
+
+public class RawQueryResultTest {
+}
